76b7ba06cdb0328593c88f89d0d005d92f4aaf5b
Snubig1 (steamdeck) <snubig1@gmail.com>
2026-03-04 19:29:27 +0100

    moved the araucaria tree configured feature to the correct namespace

--- src/main/java/net/team_phytochorion/phytochorion/world/feature/PhytochorionConfiguredFeatures.java
+++ src/main/java/net/team_phytochorion/phytochorion/world/feature/PhytochorionConfiguredFeatures.java
@@ -1,15 +1,17 @@
 package net.team_phytochorion.phytochorion.world.feature;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.worldgen.features.FeatureUtils;
+
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraftforge.registries.DeferredRegister;
 import net.team_phytochorion.phytochorion.Phytochorion;
 
 
 public class PhytochorionConfiguredFeatures {
+    public static final ResourceKey<ConfiguredFeature<?, ?>> ARAUCARIA = getKey(Phytochorion.MOD_ID,"araucaria");
 
-    public static final DeferredRegister<ConfiguredFeature<?,?>> CONFIGURED_FEATURES = DeferredRegister.create(Registries.CONFIGURED_FEATURE, Phytochorion.MOD_ID);
-    public static final ResourceKey<ConfiguredFeature<?, ?>> ARAUCARIA = FeatureUtils.createKey("araucaria");
+    private static ResourceKey<ConfiguredFeature<?, ?>> getKey(String mod_id, String name){
+        return(ResourceKey.create(Registries.CONFIGURED_FEATURE, ResourceLocation.fromNamespaceAndPath(mod_id, name)));
+    }
 }
