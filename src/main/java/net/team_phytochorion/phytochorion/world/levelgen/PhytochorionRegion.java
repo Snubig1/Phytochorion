1a4ba375a0e62ea2ef7dc849bf66306fcc239e75
Peter_Guardian <rasmusawnielsen@hotmail.com>
2026-03-26 18:36:12 +0100

    fixed araucaria forest generation

--- src/main/java/net/team_phytochorion/phytochorion/world/levelgen/PhytochorionRegion.java
+++ src/main/java/net/team_phytochorion/phytochorion/world/levelgen/PhytochorionRegion.java
@@ -6,14 +6,15 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Climate;
-import terrablender.api.ParameterUtils;
 import terrablender.api.Region;
 import terrablender.api.RegionType;
+import terrablender.api.VanillaParameterOverlayBuilder;
 
-import java.util.List;
 import java.util.function.Consumer;
 
-public class PhytochorionRegion  extends Region
+import static terrablender.api.ParameterUtils.*;
+
+public class PhytochorionRegion extends Region
 {
     public PhytochorionRegion(ResourceLocation name, int weight)
     {
@@ -23,17 +24,19 @@ public class PhytochorionRegion  extends Region
     @Override
     public void addBiomes(Registry<Biome> registry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper)
     {
-        this.addModifiedVanillaOverworldBiomes(mapper, builder -> {
-            List<Climate.ParameterPoint> araucariaMountainside = new ParameterUtils.ParameterPointListBuilder()
-                    .temperature(ParameterUtils.Temperature.NEUTRAL, ParameterUtils.Temperature.COOL)
-                    .humidity(ParameterUtils.Humidity.NEUTRAL, ParameterUtils.Humidity.WET, ParameterUtils.Humidity.HUMID)
-                    .continentalness(ParameterUtils.Continentalness.span(ParameterUtils.Continentalness.MID_INLAND, ParameterUtils.Continentalness.FAR_INLAND))
-                    .erosion(ParameterUtils.Erosion.EROSION_1, ParameterUtils.Erosion.EROSION_2, ParameterUtils.Erosion.EROSION_3, ParameterUtils.Erosion.EROSION_4)
-                    .depth(ParameterUtils.Depth.SURFACE, ParameterUtils.Depth.FLOOR)
-                    .weirdness(ParameterUtils.Weirdness.MID_SLICE_NORMAL_ASCENDING, ParameterUtils.Weirdness.HIGH_SLICE_NORMAL_ASCENDING, ParameterUtils.Weirdness.PEAK_NORMAL, ParameterUtils.Weirdness.HIGH_SLICE_NORMAL_DESCENDING, ParameterUtils.Weirdness.MID_SLICE_NORMAL_DESCENDING, ParameterUtils.Weirdness.LOW_SLICE_NORMAL_DESCENDING, ParameterUtils.Weirdness.VALLEY, ParameterUtils.Weirdness.LOW_SLICE_VARIANT_ASCENDING, ParameterUtils.Weirdness.MID_SLICE_VARIANT_ASCENDING, ParameterUtils.Weirdness.HIGH_SLICE_VARIANT_ASCENDING, ParameterUtils.Weirdness.PEAK_VARIANT, ParameterUtils.Weirdness.HIGH_SLICE_VARIANT_DESCENDING, ParameterUtils.Weirdness.MID_SLICE_VARIANT_DESCENDING)
-                    .build();
+        VanillaParameterOverlayBuilder builder = new VanillaParameterOverlayBuilder();
+        new ParameterPointListBuilder()
+                .temperature(Temperature.span(Temperature.COOL, Temperature.NEUTRAL))
+                .humidity(Humidity.NEUTRAL, Humidity.WET, Humidity.HUMID)
+                .continentalness(Continentalness.MID_INLAND, Continentalness.FAR_INLAND)
+                .erosion(Erosion.EROSION_0, Erosion.EROSION_1, Erosion.EROSION_2)
+                .depth(Depth.SURFACE, Depth.FLOOR)
+                .weirdness(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_DESCENDING, Weirdness.MID_SLICE_NORMAL_DESCENDING, Weirdness.LOW_SLICE_NORMAL_DESCENDING)
+                .build().forEach(point -> builder.add(point, PhytochorionBiomes.ARAUCARIA_FOREST));
 
-            List<Climate.ParameterPoint> araucariaFlat = new ParameterUtils.ParameterPointListBuilder()
+        builder.build().forEach(mapper);
+
+            /*List<Climate.ParameterPoint> araucariaFlat = new ParameterUtils.ParameterPointListBuilder()
                     .temperature(ParameterUtils.Temperature.NEUTRAL, ParameterUtils.Temperature.COOL)
                     .humidity(ParameterUtils.Humidity.NEUTRAL, ParameterUtils.Humidity.WET, ParameterUtils.Humidity.HUMID)
                     .continentalness(ParameterUtils.Continentalness.span(ParameterUtils.Continentalness.MID_INLAND, ParameterUtils.Continentalness.FAR_INLAND))
@@ -44,6 +47,6 @@ public class PhytochorionRegion  extends Region
 
             araucariaMountainside.forEach(point -> builder.replaceBiome(point, PhytochorionBiomes.ARAUCARIA_FOREST));
             araucariaFlat.forEach(point -> builder.replaceBiome(point, PhytochorionBiomes.ARAUCARIA_FOREST));
-        });
+        });*/
     }
 }
