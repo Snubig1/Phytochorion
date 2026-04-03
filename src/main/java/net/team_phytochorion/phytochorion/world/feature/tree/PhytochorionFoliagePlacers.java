8537e102ee931672c1c95813f46e94a8be9c7e80
Peter_Guardian <rasmusawnielsen@hotmail.com>
2026-04-03 21:18:48 +0200

    started adding the ginkgo tree

--- src/main/java/net/team_phytochorion/phytochorion/world/feature/tree/PhytochorionFoliagePlacers.java
+++ src/main/java/net/team_phytochorion/phytochorion/world/feature/tree/PhytochorionFoliagePlacers.java
@@ -8,11 +8,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.team_phytochorion.phytochorion.Phytochorion;
 import net.team_phytochorion.phytochorion.world.feature.tree.Custom.AraucariaFoliagePlacer;
+import net.team_phytochorion.phytochorion.world.feature.tree.Custom.GinkgoFoliagePlacer;
 
 public class PhytochorionFoliagePlacers {
     public static final DeferredRegister<FoliagePlacerType<?>> FOLIAGE_PLACERS = DeferredRegister.create(ForgeRegistries.FOLIAGE_PLACER_TYPES, Phytochorion.MOD_ID);
 
     public static final RegistryObject<FoliagePlacerType<AraucariaFoliagePlacer>> ARAUCARIA_FOLIAGE_PLACER = FOLIAGE_PLACERS.register("araucaria_foliage_placer", () -> new FoliagePlacerType<>(AraucariaFoliagePlacer.CODEC));
+    public static final RegistryObject<FoliagePlacerType<GinkgoFoliagePlacer>> GINKGO_FOLIAGE_PLACER = FOLIAGE_PLACERS.register("ginkgo_foliage_placer", () -> new FoliagePlacerType<>(GinkgoFoliagePlacer.CODEC));
 
     public static void register(IEventBus eventBus) {
         FOLIAGE_PLACERS.register(eventBus);
