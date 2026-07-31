b44a1aef1b929ec42bb0a51dfdb61595930be002
Peter_Guardian <rasmusawnielsen@hotmail.com>
2026-07-31 19:51:46 +0200

    update sapped icon textures and added dried ghost pipe item

--- src/main/java/net/team_phytochorion/phytochorion/items/PhytochorionItems.java
+++ src/main/java/net/team_phytochorion/phytochorion/items/PhytochorionItems.java
@@ -19,6 +19,8 @@ public class PhytochorionItems {
     public static final RegistryObject<Item> GINKGO_SIGN = ITEMS.register("ginkgo_sign", () -> new SignItem(new Item.Properties().stacksTo(16), PhytochorionBlocks.GINKGO_SIGN.get(), PhytochorionBlocks.GINKGO_WALL_SIGN.get()));
     public static final RegistryObject<Item> GINKGO_HANGING_SIGN = ITEMS.register("ginkgo_hanging_sign", () -> new HangingSignItem(PhytochorionBlocks.GINKGO_HANGING_SIGN.get(), PhytochorionBlocks.GINKGO_WALL_HANGING_SIGN.get(), new Item.Properties().stacksTo(16)));
 
+    public static final RegistryObject<Item> DRIED_GHOST_PIPE = ITEMS.register("dried_ghost_pipe", () -> new Item(new Item.Properties()));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
--- src/main/java/net/team_phytochorion/phytochorion/misc/PhytochorionCreativeModeTabs.java
+++ src/main/java/net/team_phytochorion/phytochorion/misc/PhytochorionCreativeModeTabs.java
@@ -65,6 +65,7 @@ public class PhytochorionCreativeModeTabs {
                         pOutput.accept(PhytochorionBlocks.BUTTERFLY_WEED.get());
                         pOutput.accept(PhytochorionBlocks.GHOST_PIPE.get());
                         pOutput.accept(PhytochorionBlocks.RED_GHOST_PIPE.get());
+                        pOutput.accept(PhytochorionItems.DRIED_GHOST_PIPE.get());
                     })
                     .build());
 
